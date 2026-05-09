095eae9666f5b816a5594f5262502c2299228c21
Akhere Ihoeghinlan <101akhere5@gmail.com>
2026-05-09 12:45:10 +0100

    created page structure

---
+++ frontend/src/app/(dashboard)/routines/make/page.tsx
@@ -0,0 +1,3 @@
+export default function CreateRoutine() {
+  return <div>CreateRoutine</div>;
+}
---
+++ frontend/src/app/(dashboard)/workouts/page.tsx
@@ -0,0 +1,3 @@
+export default function WorkoutsCalendar() {
+  return <div>WorkoutsCalendar</div>;
+}
