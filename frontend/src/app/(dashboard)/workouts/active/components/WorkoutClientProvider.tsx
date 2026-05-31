dac416354374b694075d51b5b169e6aa0833627a
Akhere Ihoeghinlan <101akhere5@gmail.com>
2026-05-31 19:12:57 +0100

    added notification when rest time completes

--- frontend/src/app/(dashboard)/workouts/active/components/WorkoutClientProvider.tsx
+++ frontend/src/app/(dashboard)/workouts/active/components/WorkoutClientProvider.tsx
@@ -8,6 +8,9 @@ import { Routine } from "@/src/features/routines/types";
 import { Exercise, useWorkout } from "@/src/features/workout/store";
 import ExerciseList from "./ExerciseList";
 import { getDefaultSets } from "@/src/features/workout/utils";
+import { useTimer } from "@/src/features/timer/store";
+import { toast } from "sonner";
+import { Toaster } from "@/src/components/ui/sonner";
 
 export default function WorkoutClientProvider({
   routine,
@@ -15,6 +18,7 @@ export default function WorkoutClientProvider({
   routine: Routine | null;
 }) {
   const { setWorkoutData } = useWorkout();
+  const { currentTime, isRunning } = useTimer();
 
   useEffect(() => {
     if (routine) {
@@ -39,9 +43,19 @@ export default function WorkoutClientProvider({
     }
   }, [routine, setWorkoutData]);
 
+  useEffect(() => {
+    console.log(currentTime);
+    if (currentTime === 0 && isRunning) {
+      toast("Rest complete!");
+      // TODO: Add sound
+    }
+  }, [currentTime, isRunning]);
+
   return (
     <div className="relative  h-screen flex flex-1 flex-col justify-between">
       <div>
+        <Toaster position="top-right" richColors closeButton />
+
         <ExerciseList />
         <ExerciseDetails />
         <SetList routine={routine} />
