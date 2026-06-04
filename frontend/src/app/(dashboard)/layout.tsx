e90cf7124d4076944d32496214974d488cd11f4b
Akhere Ihoeghinlan <101akhere5@gmail.com>
2026-06-04 18:53:00 +0100

    added navbar

---
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,74 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/src/components/ui/navigation-menu";
+import "../globals.css";
+
+import { Calendar, Cog, House } from "lucide-react";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard for your exercises",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        {children}
+
+        <div className="sticky bottom-0 w-screen">
+          <NavigationMenu className="bg-secondary/95 w-screen  ">
+            <NavigationMenuList className="gap-8 w-screen">
+              <NavigationMenuItem>
+                <NavigationMenuLink>
+                  <Link href="/routines">
+                    <House style={{ width: "24px", height: "24px" }} />
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuLink>
+                  <Link href="/workouts">
+                    <Calendar style={{ width: "24px", height: "24px" }} />
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuLink>
+                  <Link href="/settings">
+                    <Cog style={{ width: "24px", height: "24px" }} />
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </body>
+    </html>
+  );
+}
